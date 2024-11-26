0823bd2b0440844febc73c315cce3938f6b2e8a3
Christian Llontop <chrisllontop@icloud.com>
2024-11-26 12:45:26 -0500

    Replace Rspack with Webpack config

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
-import { defineConfig } from "@rspack/cli";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,33 +23,32 @@ const commonConfig = {
       "@": resolve(__dirname, "src"),
     },
   },
-  // https://webpack.js.org/configuration/externals/ - Libraries required by OpenFeature SDK
   externals: {
-    'node:events': 'commonjs events',
-    'node:crypto': 'commonjs crypto',
-    'async_hooks': 'commonjs async_hooks',
-  }
+    "node:events": "commonjs events",
+    "node:crypto": "commonjs crypto",
+    "async_hooks": "commonjs async_hooks",
+  },
 };
 
-const esmConfig = defineConfig({
+const esmConfig = {
   ...commonConfig,
   output: {
-    path: "./dist",
+    path: resolve(__dirname, "dist"),
     filename: "index.esm.js",
     libraryTarget: "module",
   },
   experiments: {
     outputModule: true,
   },
-});
+};
 
-const cjsConfig = defineConfig({
+const cjsConfig = {
   ...commonConfig,
   output: {
-    path: "./dist",
+    path: resolve(__dirname, "dist"),
     filename: "index.cjs.js",
     libraryTarget: "commonjs2",
   },
-});
+};
 
 export default [esmConfig, cjsConfig];
