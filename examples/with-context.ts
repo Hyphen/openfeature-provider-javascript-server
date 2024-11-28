0037f5d4234a82d49e6946884802dca966d7db05
Christian Llontop <chrisllontop@icloud.com>
2024-11-28 17:50:38 -0500

    Add example demonstrating context usage with OpenFeature

---
+++ examples/with-context.ts
@@ -0,0 +1,37 @@
+import { OpenFeature } from '@openfeature/server-sdk';
+import  { HyphenProvider, type HyphenProviderOptions, type HyphenEvaluationContext } from 'openfeature-provider-javascript-server';
+
+const publicKey = "your-public-key";
+
+const options: HyphenProviderOptions = {
+  application: 'application-id',
+  environment: 'production',
+}
+
+const context: HyphenEvaluationContext = {
+  targetingKey: 'target-key-1',
+  ipAddress: '203.0.113.42',
+  customAttributes: {
+    subscriptionLevel: 'premium',
+    region: 'us-east',
+  },
+  user: {
+    id: 'user-123',
+    email: 'user@example.com',
+    name: 'John Doe',
+    customAttributes: {
+      role: 'admin',
+    },
+  },
+};
+
+// Register your feature flag provider
+await OpenFeature.setProviderAndWait(new HyphenProvider(publicKey, options));
+
+// create a new client
+const client = OpenFeature.getClient();
+
+// Evaluate your feature flag
+const data = await client.getBooleanDetails('my-bool-toggle', false, context);
+
+console.log('Data', data.value); // true or false
