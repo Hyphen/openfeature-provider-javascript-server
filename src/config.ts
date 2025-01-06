e17ded95f5903101459808c63c6197e6f48a2fb0
Christian Llontop <chrisllontop@icloud.com>
2025-01-06 11:36:58 -0500

    fix: refactor API request handling (#18)

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,6 @@ export const horizon = {
   url: `https://horizon.hyphen.ai`,
 };
 
-export const horizonEndpoints = {
-  evaluate: `${horizon.url}/toggle/evaluate`,
-  telemetry: `${horizon.url}/toggle/telemetry`,
-};
-
 export const cache = {
   ttlSeconds: process.env.CACHE_TTL_SECONDS ? parseInt(process.env.CACHE_TTL_SECONDS) : 30,
 };
